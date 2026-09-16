2e54152f1892df6f1c2c2dca264f2423bbd20751
AKIRA <1akiraforp@gmail.com>
2026-09-16 21:33:54 +0530

    Day 1 of Practising CPP

---
+++ LearnCPP/1.5.2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x {}, y {}, z {};
+    cout << "Enter three integers: ";
+    cin >> x >> y >> z;
+    cout << "You entered: " << x << ", " << y << ", " << z << "\n";
+    // For testing purpose Not the part of the quiz
+    // std::cout << "Hello
+    // world!"; // Not allowed!
+    // std::cout << "Hello "
+    // "world!"; // prints "Hello world!"
+    system("pause");
+    return 0;
+}
